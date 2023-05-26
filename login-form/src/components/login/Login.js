c92e5f173b7719ce2c093a7c8c9d28f1ee038eeb
Nhoj <nhojplummer@yahoo.com>
2023-05-26 13:42:18 -0400

    Starting to make progress on code

--- login-form/src/components/login/Login.js
+++ login-form/src/components/login/Login.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const [errorMessages, setErrorMessages] = useState({})
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
   return (
     <div>Login</div>
   )
